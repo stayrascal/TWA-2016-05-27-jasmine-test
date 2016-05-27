73047fb883cdf53ea07130b37199aed41bb1df8a
rascal <stayrascal@gmail.com>
2016-05-27 15:05:05 +0800

    fix bug

--- app/score.js
+++ app/score.js
@@ -1,5 +1,12 @@
-exports.transferScoreToGrade = function (score, scoreObj) {
-    var section = score - score % scoreObj.step;
-    return scoreObj[section] || scoreObj.default;
+exports.transferScoreToGrade = function (score) {
+    var scoreSections = [90, 80, 60];
+    var label = ['A', 'B', 'C', 'D'];
+
+    for(var i = 0; i < scoreSections.length; i++){
+        if (score >= scoreSections[i]){
+            return label[i];
+        }
+    }
+    return label[i];
 };
 
--- spec/scoreSpec.js
+++ spec/scoreSpec.js
@@ -1,28 +1,28 @@
 var score = require('../app/score.js');
 
 describe('Score Test', function () {
-
-    var gradeObj = {
-        90: 'A',
-        80: 'B',
-        60: 'C',
-        default: 'D',
-        step: 10
-
-    };
-
     it('Should return A when input 95', function () {
-        var expectGrade = score.transferScoreToGrade(95, gradeObj);
+        var expectGrade = score.transferScoreToGrade(95);
         expect(expectGrade).toBe('A');
     });
 
-    it('Should return A when input 80', function () {
-        var expectGrade = score.transferScoreToGrade(80, gradeObj);
+    it('Should return A when input 90', function () {
+        var expectGrade = score.transferScoreToGrade(90);
+        expect(expectGrade).toBe('A');
+    });
+
+    it('Should return B when input 80', function () {
+        var expectGrade = score.transferScoreToGrade(80);
         expect(expectGrade).toBe('B');
     });
 
-    it('Should return A when input 55', function () {
-        var expectGrade = score.transferScoreToGrade(55, gradeObj);
+    it('Should return C when input 60', function () {
+        var expectGrade = score.transferScoreToGrade(60);
+        expect(expectGrade).toBe('C');
+    });
+
+    it('Should return D when input 55', function () {
+        var expectGrade = score.transferScoreToGrade(55);
         expect(expectGrade).toBe('D');
     });
 });
